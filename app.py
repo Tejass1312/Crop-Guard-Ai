44f927e0a01aceca8248842abf0e5396c3b3bedc
Tejas Somwanshi <tsomwanshi1312@gmail.com>
2026-02-23 11:20:40 +0530

    fixed render port

--- app.py
+++ app.py
@@ -81,7 +81,8 @@ def predict_api():
         "risk_analysis": risk
     })
 
-
 # ---------------- RUN APP ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
